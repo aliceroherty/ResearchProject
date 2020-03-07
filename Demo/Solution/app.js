513e70a10567a77ccf15caa2bb761c728ae19129
aliceroherty <aliceroherty@gmail.com>
2020-03-06 23:03:23 -0400

    Created Demo for Completion During Presentation

---
+++ Demo/Solution/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use("/assets", express.static("assets"))
+
+/*app.get("/", (request, response) => {
+    response.sendFile(`${__dirname}/views/index.html`);
+});*/
+
+app.get("/", (request, response) => {
+    response.render("index", { message: "Hello World!" });
+});
+
+app.get("/users/:id", (request, response) => {
+    response.send(request.params.id);
+});
+
+app.get("/form", (request, response) => {
+    response.sendFile(`${__dirname}/views/form.html`);
+});
+
+app.post("/form", urlEncodedParser, (request, response) => {
+    response.send(request.body);
+    console.log(request.body.firstName);
+    console.log(request.body.lastName);
+});
+
+app.listen(80, () => {
+    console.log("The server is listening on port 80...");
+});
